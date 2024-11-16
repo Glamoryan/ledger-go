813a56fc36f8c36b07c0729a097e457987416a76
Adnan Ozdemir <adnan.ozdemir@useinsider.com>
2024-11-16 15:35:45 +0300

    Handler updated & Testify framework added

--- src/handlers/user_handler.go
+++ src/handlers/user_handler.go
@@ -5,6 +5,7 @@ import (
 	"Ledger/src/validation"
 	"encoding/json"
 	"net/http"
+	"strconv"
 )
 
 type UserHandler struct {
@@ -50,4 +51,23 @@ func (h *UserHandler) GetAllUsers(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(users)
+}
+
+func (h *UserHandler) GetUserByID(w http.ResponseWriter, r *http.Request) {
+	vars := r.URL.Query().Get("id")
+
+	userID, err := strconv.Atoi(vars)
+
+	if err != nil {
+		WriteErrorResponse(w, http.StatusBadRequest, "Invalid user ID format")
+	}
+
+	user, err := h.service.GetUserByID(uint(userID))
+
+	if err != nil {
+		WriteErrorResponse(w, http.StatusNotFound, "User not found")
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(user)
 }
