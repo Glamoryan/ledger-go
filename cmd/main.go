d5518b340da99e3cc5d20041ee0a77a5594295ec
Adnan Ozdemir <adnan.ozdemir@useinsider.com>
2025-01-13 18:01:17 +0300

    fix: remove auto migrate and add admin middleware

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"Ledger/pkg/db"
 	"Ledger/src/factory"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -17,24 +18,25 @@ func main() {
 
 	router := mux.NewRouter()
 
+	// Public endpoints
 	router.HandleFunc("/users/add-user", userHandler.CreateUser).Methods("POST")
 	router.HandleFunc("/login", userHandler.Login).Methods("POST")
 
-	router.HandleFunc("/users", authMiddleware.Authenticate(userHandler.GetAllUsers)).Methods("GET")
-	router.HandleFunc("/users/get-user", authMiddleware.Authenticate(userHandler.GetUserByID)).Methods("GET")
+	// Protected endpoints
 	router.HandleFunc("/users/get-credit", authMiddleware.Authenticate(userHandler.GetCredit)).Methods("GET")
 	router.HandleFunc("/users/send-credit", authMiddleware.Authenticate(userHandler.SendCredit)).Methods("POST")
 	router.HandleFunc("/users/send-credit-async", authMiddleware.Authenticate(userHandler.SendCreditAsync)).Methods("POST")
 	router.HandleFunc("/users/transaction-logs/sender", authMiddleware.Authenticate(userHandler.GetTransactionLogsBySenderAndDate)).Methods("GET")
 
+	// Admin only endpoints
+	router.HandleFunc("/users", authMiddleware.Authenticate(authMiddleware.AdminOnly(userHandler.GetAllUsers))).Methods("GET")
+	router.HandleFunc("/users/get-user", authMiddleware.Authenticate(authMiddleware.AdminOnly(userHandler.GetUserByID))).Methods("GET")
 	router.HandleFunc("/users/add-credit", authMiddleware.Authenticate(authMiddleware.AdminOnly(userHandler.AddCredit))).Methods("POST")
 	router.HandleFunc("/users/credits", authMiddleware.Authenticate(authMiddleware.AdminOnly(userHandler.GetAllCredits))).Methods("GET")
-
 	router.HandleFunc("/users/batch/credits", authMiddleware.Authenticate(authMiddleware.AdminOnly(userHandler.GetMultipleUserCredits))).Methods("POST")
 	router.HandleFunc("/users/batch/update-credits", authMiddleware.Authenticate(authMiddleware.AdminOnly(userHandler.ProcessBatchCreditUpdate))).Methods("POST")
 
-	err := http.ListenAndServe(":8080", router)
-	if err != nil {
-		return
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
 	}
 }
